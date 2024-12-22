c43ab02097bd6952d3e335f4ed938d3cb93d5fdb
SudharMurugan <sudharshan100002@gmail.com>
2024-12-23 02:42:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // Array of product details
 const products = [
-    { title: "Product 1", image: "https://via.placeholder.com/150", description: "Description of Product 1" },
-    { title: "Product 2", image: "https://via.placeholder.com/150", description: "Description of Product 2" },
-    { title: "Product 3", image: "https://via.placeholder.com/150", description: "Description of Product 3" },
-    { title: "Product 4", image: "https://via.placeholder.com/150", description: "Description of Product 4" },
-    { title: "Product 5", image: "https://via.placeholder.com/150", description: "Description of Product 5" },
-    { title: "Product 6", image: "https://via.placeholder.com/150", description: "Description of Product 6" },
-    { title: "Product 7", image: "https://via.placeholder.com/150", description: "Description of Product 7" },
-    { title: "Product 8", image: "https://via.placeholder.com/150", description: "Description of Product 8" },
-    { title: "Product 9", image: "https://via.placeholder.com/150", description: "Description of Product 9" },
-    { title: "Product 10", image: "https://via.placeholder.com/150", description: "Description of Product 10" },
-    { title: "Product 11", image: "https://via.placeholder.com/150", description: "Description of Product 11" },
-    { title: "Product 12", image: "https://via.placeholder.com/150", description: "Description of Product 12" },
-    { title: "Product 13", image: "https://via.placeholder.com/150", description: "Description of Product 13" },
-    { title: "Product 14", image: "https://via.placeholder.com/150", description: "Description of Product 14" }
+    { title: "Product 1", image: "Images/image-baklava-desktop.jpg", description: "Description of Product 1" },
+    { title: "Product 2", image: "Images/image-brownie-tablet.jpg", description: "Description of Product 2" },
+    { title: "Product 3", image: "Images/image-cake-desktop.jpg", description: "Description of Product 3" },
+    { title: "Product 4", image: "Images/image-creme-brulee-tablet.jpg", description: "Description of Product 4" },
+    { title: "Product 5", image: "Images/image-macaron-desktop.jpg", description: "Description of Product 5" },
+    { title: "Product 6", image: "Images/image-meringue-tablet.jpg", description: "Description of Product 6" },
+    { title: "Product 7", image: "Images/image-panna-cotta-desktop.jpg", description: "Description of Product 7" },
+    { title: "Product 8", image: "Images/image-tiramisu-tablet.jpg", description: "Description of Product 8" },
+    { title: "Product 9", image: "Images/images (1).jpg", description: "Description of Product 9" },
+    { title: "Product 10", image: "Images/images (2).jpg", description: "Description of Product 10" },
+    { title: "Product 11", image: "Images/images (3).jpg", description: "Description of Product 11" },
+    { title: "Product 12", image: "Images/images (4).jpg", description: "Description of Product 12" },
+    { title: "Product 13", image: "Images/images (5).jpg", description: "Description of Product 13" },
+    { title: "Product 14", image: "Images/images (5).jpg", description: "Description of Product 14" },
+    { title: "Product 15", image: "Images/images (5).jpg", description: "Description of Product 15" },
+    { title: "Product 16", image: "Images/images.jpg", description: "Description of Product 16" }
+
 ];
 
 
